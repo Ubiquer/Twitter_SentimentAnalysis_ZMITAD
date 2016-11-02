5f1f057da361f41fd42dfd78c76e9fe4b0774c94
Arek <lysakowski3@gmail.com>
2016-11-02 17:09:34 +0100

    commit

--- TweetStreamer.py
+++ TweetStreamer.py
@@ -54,15 +54,27 @@ documents = [(list(movie_reviews.words(fileid)), category)
 
 random.shuffle(documents)
 
-print(documents[1])
-
 all_words = []
 
 for w in movie_reviews.words():
     all_words.append(w.lower())
 
 all_words = nltk.FreqDist(all_words)
-print(all_words.most_common(15))
+
+word_features = list(all_words.keys())[:3000]
+
+def find_features(document):
+    words = set(document)
+    features = {}
+    for w in word_features:
+        features[w] = (w in words)  #so if the key that is the word in 3000 most popular words is gonna be equal to
+
+    return features
+
+print((find_features(movie_reviews.words('neg/cv000_29416.txt'))))
+
+featuresets = [(find_features(rev), category) for (rev, category) in documents]
+
 
 
 
